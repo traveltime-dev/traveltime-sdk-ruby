eee7fc1145c1481e99e8ae420cf9f0880714ed3c
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2022-02-14 18:03:23 +0000

    Make CI pass

--- spec/travel_time_spec.rb
+++ spec/travel_time_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe TravelTime do
   it 'has a version number' do
     expect(TravelTime::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
