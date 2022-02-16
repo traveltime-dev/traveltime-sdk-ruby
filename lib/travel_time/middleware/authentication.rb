d1f20e7e69562f9eebd16698cc32a0b9c79143b9
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2022-02-16 17:05:22 +0000

    Add tests for TravelTime::Middleware::Authentication.

--- lib/travel_time/middleware/authentication.rb
+++ lib/travel_time/middleware/authentication.rb
@@ -7,9 +7,12 @@ module TravelTime
     # The Authentication middleware is responsible for setting the auth headers
     # on each request. These are automatically taken from the `TravelTime.config`.
     class Authentication < Faraday::Middleware
+      APP_ID_HEADER = 'X-Application-Id'
+      API_KEY_HEADER = 'X-Api-Key'
+
       def on_request(env)
-        env.request_headers['X-Application-Id'] = TravelTime.config.application_id
-        env.request_headers['X-Api-Key'] = TravelTime.config.api_key
+        env.request_headers[APP_ID_HEADER] = TravelTime.config.application_id
+        env.request_headers[API_KEY_HEADER] = TravelTime.config.api_key
       end
 
       def map_info
---
+++ spec/travel_time/middleware/authentication_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe TravelTime::Middleware::Authentication do
+  let(:faraday_env) do
+    Faraday::Env.new.tap do |env|
+      env.request_headers = Faraday::Utils::Headers.new
+    end
+  end
+  let(:middleware) { described_class.new }
+
+  it 'automatically fetches the application_id from the configuration and set it for the request' do
+    middleware.on_request(faraday_env)
+    value = faraday_env.request_headers[described_class::APP_ID_HEADER]
+    expected = TravelTime.config.application_id
+    expect(value).to eq(expected)
+  end
+
+  it 'automatically fetches the api_key from the configuration and set it for the request' do
+    middleware.on_request(faraday_env)
+    value = faraday_env.request_headers[described_class::API_KEY_HEADER]
+    expected = TravelTime.config.api_key
+    expect(value).to eq(expected)
+  end
+end
