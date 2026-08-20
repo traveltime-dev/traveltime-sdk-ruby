a9dcc4ee60f536f1402dda8b58d60de027f63dca
arnasbr <arnas.bradauskas@traveltime.com>
2026-08-20 15:51:27 +0300

    Add unions and intersections support to time-map fast

--- lib/travel_time/client.rb
+++ lib/travel_time/client.rb
@@ -125,9 +125,11 @@ module TravelTime
       perform_request { connection.post('distance-map', payload, { 'Accept' => format }) }
     end
 
-    def time_map_fast(arrival_searches:, format: nil)
+    def time_map_fast(arrival_searches:, unions: nil, intersections: nil, format: nil)
       payload = {
-        arrival_searches: arrival_searches
+        arrival_searches: arrival_searches,
+        unions: unions,
+        intersections: intersections
       }.compact
       perform_request { connection.post('time-map/fast', payload, { 'Accept' => format }) }
     end
